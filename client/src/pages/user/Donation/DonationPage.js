cc05e200d4ef2ca99e06ae9ba4c46f3aa9319e0d
Jurome <jaspherodevilas.09@gmail.com>
2024-02-26 10:49:13 +0800

    UI Sidebar functions and User Profile

--- client/src/pages/user/Donation/DonationPage.js
+++ client/src/pages/user/Donation/DonationPage.js
@@ -1,216 +1,176 @@
 import React, { useState, useEffect } from 'react';
 import {
-    MDBContainer as Container,
-    MDBRow as Row,
-    MDBCol as Col,
-    MDBCard as Card,
-    MDBCardBody as CardBody,
-    MDBCardTitle as CardTitle,
-    MDBBtn, MDBModal, MDBModalDialog, MDBModalContent, MDBModalHeader, MDBModalTitle, MDBModalBody, MDBModalFooter,
+  MDBContainer as Container,
+  MDBRow as Row,
+  MDBCol as Col,
+  MDBCarousel,
+  MDBCarouselItem,
+  MDBCard,
+  MDBCardImage,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText
 } from 'mdb-react-ui-kit';
-// import Header from '../components/Layouts/Header';
-import Header from '../../../components/Layouts/Header'
+import Header from '../../../components/Layouts/Header';
 import Sidebar from '../../../components/Layouts/Sidebar';
-import InputType from '../../../components/Shared/Form/InputType'
 import '../../../index.css';
-import { useSelector } from 'react-redux'
-import axios from 'axios'
-import 'mdb-react-ui-kit'
+import { useSelector } from 'react-redux';
+import axios from 'axios';
 
-const DonationPage = () => {
+const Campain = () => {
+  const [events, setEvents] = useState([]);
+  const [error, setError] = useState('');
 
-    const [donateModal, setDonateModal] = useState(false);
-    const toggleOpen = () => setDonateModal(!donateModal);
+  const { user } = useSelector(state => state.user);
 
-    const [inventoryType, setInventoryType] = useState('in');
-    const [bloodGroup, setBloodGroup] = useState('');
-    const [quantity, setQuantity] = useState('');
-    const [donorEmail, setDonorEmail] = useState('');
-    const [success, setSuccess] = useState('');
-    const [events, setEvents] = useState([]);
-    const [loading, setLoading] = useState('');
-    const [error, setError] = useState('')
+  const token = localStorage.getItem('token');
 
-    const { user } = useSelector(state => state.user)
-
-    // console.log(events)
-
-    const token = localStorage.getItem('token')
-    // console.log(token)
-    const getAllEvents = async () => {
-        try {
-
-            const config = {
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${token}`
-                }
-            }
-            const { data } = await axios.get(`${process.env.REACT_APP_BASEURL}/event/getAllEvents`, config)
-            // console.log(data)
-            setEvents(data.data); // Set events using data.data
-            setLoading(false)
-        } catch (error) {
-            setError(error.response.data.message)
+  const getAllEvents = async () => {
+    try {
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
         }
+      };
+      const { data } = await axios.get(`${process.env.REACT_APP_BASEURL}/event/getAllEvents`, config);
+      setEvents(data.data);
+    } catch (error) {
+      setError(error.response.data.message);
     }
+  };
 
-    useEffect(() => {
-        getAllEvents();
-    }, [error]);
+  useEffect(() => {
+    getAllEvents();
+  }, []);
 
-    return (
-        <>
-            <div className="custom-homepage my-5">
-                <Header sticky />
-                <div className="custom-content">
-                    <Container fluid>
-                        <Row>
-                            <Col md={2}>
-                                <Sidebar />
-                            </Col>
-                            <Col md={10}>
-                                <Row className="mb-4">
-                                    <div className="container">
-                                        <div style={{ display: 'flex', alignItems: 'center' }}>
-                                            <img src="./assets/images/systemLOGOMAIN.png" alt="logotup" id='tuplogo' style={{ width: "20%", height: "20%" }} />
-                                            <div style={{ marginLeft: '20px', padding: '20px', background: '#f0f0f0', border: '2px solid #333', borderRadius: '10px', boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)' }}>
-                                                <p style={{ fontWeight: 'bold', fontSize: '24px', textAlign: 'center' }}>The Donations are HERE!</p>
-                                                <p style={{ textAlign: 'center' }}>
-                                                    This will be the page that handles blood donation events that the local health centers post. We will be cooperating by creating an efficient way to process the blood donation.
-                                                </p>
-                                                {/* <p style={{ textAlign: 'center' }}>- A Comprehensive Study</p> */}
-                                            </div>
-                                        </div>
-                                    </div>
-                                </Row>
-                                <Row className="mb-4">
-                                    <div className="container">
-                                        <div style={{ textAlign: 'center', fontWeight: 'bold', fontSize: '24px', padding: '20px', background: '#f0f0f0', border: '2px solid #333', borderRadius: '10px', boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)' }}>
-                                            <p style={{ margin: '0' }}>DONATIONS</p>
-                                        </div>
-                                    </div>
-                                    {events && events.map(event => (
-                                        event.eventType === 'donation' && (
-                                            <Col md={6} sm={12} className="custom-card-column my-2" key={event._id}>
-                                                <Card style={{ height: '450px', width: '100%' }}>
-                                                    <CardBody>
-                                                        <CardTitle className="custom-card-title" onClick={toggleOpen}>
-                                                            <i className="fa-solid fa-plus" style={{ cursor: "pointer" }}></i> Donate
-                                                        </CardTitle>
-                                                        <div style={{ display: 'flex', alignItems: 'center' }}>
-                                                            {event.images && event.images.map(image => (
-                                                                <img key={image.public_id} src={image.url} alt={event.title} className="event-image" style={{ marginRight: '10px' }} />
-                                                            ))}
-                                                            <div>
-                                                                <p className="custom-card-description" style={{ fontWeight: 'bold', color: 'black' }}>Title: <p style={{ fontWeight: 'bold', color: 'red' }}>{event.title}</p></p>
-                                                                <p className="custom-card-description">{event.date}</p>
-                                                                <p className="custom-card-description" style={{ fontWeight: 'bold', color: 'black' }}>Place<p style={{ fontWeight: 'bold', color: 'red' }}>{event.place}</p></p>
-                                                                <p className="custom-card-description">{event.status}</p>
-                                                            </div>
-                                                        </div>
-                                                        <p style={{ fontWeight: 'bold' }}>Details:</p>
-                                                        <p className="custom-card-description">{event.details}</p>
-                                                    </CardBody>
-                                                </Card>
-                                            </Col>
-                                        )
-                                    ))}
-                                </Row>
-                            </Col>
-                        </Row>
+  return (
+    <>
+      <Header sticky />
+      <Sidebar/>
+      <div style={{display: 'flex', justifyContent: 'flex-end', alignItems: 'center', height: '75vh'}}>
+        <img src="./assets/images/DONATION.png" classname="img-fluid" alt="banner" style={{width: '750%', height: '75%', objectFit: 'cover'}} />
+      </div>
+      <Container>
+        <Row>
+          <Col md={8} className="offset-md-1">
+            {/* <MDBCarousel showControls showIndicators dark fade style={{ width: '1300px', height: '500px' }}>
+              <MDBCarouselItem itemId={1}>
+              <img
+                  className='d-block w-50'
+                  src='./assets/images/CAMPAIN.png' // Use your image as the src
+                  alt='logotup' // Add alt text for accessibility
+                  id='tuplogo'
+                />
+              </MDBCarouselItem>
+              <MDBCarouselItem itemId={2}>
+                <img
+                  className='d-block w-100'
+                  src='https://mdbootstrap.com/img/Photos/Slides/img%20(20).jpg'
+                  alt='Third slide'
+                />
+              </MDBCarouselItem>
+            </MDBCarousel> */}
+            <div>
+              <hr className="hr" style={{height: 4, width: 1250, backgroundColor: 'black'}} />
+              <hr className="hr" style={{height: 4, width: 1250, backgroundColor: 'black'}} />
+            </div>
 
-                        <MDBModal tabIndex="-1" open={donateModal} setOpen={setDonateModal}>
-                            <MDBModalDialog centered size="">
-                                <MDBModalContent>
-                                    <MDBModalHeader>
-                                        <MDBModalTitle>Donate</MDBModalTitle>
-                                        <MDBBtn
-                                            className="btn-close"
-                                            color="none"
-                                            onClick={toggleOpen}
-                                        ></MDBBtn>
-                                    </MDBModalHeader>
-                                    <MDBModalBody>
-                                        <div className="d-flex">
-                                            Blood Type: &nbsp;
-                                            <div className='form-check ms-3'>
-                                                <input type='radio'
-                                                    name='inRadio'
-                                                    defaultChecked
-                                                    value={'in'}
-                                                    onChange={(e) => setInventoryType(e.target.value)}
-                                                    className='form-check-input' />
-                                                <label htmlFor='in' className='form-check-label'>
-                                                    IN
-                                                </label>
-                                            </div>
-                                            <div className='form-check ms-3'>
-                                                <input type='radio'
-                                                    name='inRadio'
-                                                    value={'out'}
-                                                    onChange={(e) => setInventoryType(e.target.value)}
-                                                    className='form-check-input' />
-                                                <label htmlFor='out' className='form-check-label'>
-                                                    OUT
-                                                </label>
-                                            </div>
-                                        </div>
-                                        {user && (
-                                            <>
-                                                <select className="form-select"
-                                                    aria-label="Default select example"
-                                                    onChange={(e) => setBloodGroup(e.target.value)}
-                                                >
-                                                    {user && user.description && user.description.length > 0 ? (
-                                                        <option defaultValue value={user.description[0].bloodType}>{user.description[0].bloodType}</option>
-                                                    ) : (
-                                                        <>
-                                                            <option selected>Select</option>
-                                                            <option value={'O+'}>O+</option>
-                                                            <option value={'O-'}>O-</option>
-                                                            <option value={'A+'}>A+</option>
-                                                            <option value={'A-'}>A-</option>
-                                                            <option value={'B+'}>B+</option>
-                                                            <option value={'B-'}>B-</option>
-                                                            <option value={'AB+'}>AB+</option>
-                                                            <option value={'AB-'}>AB-</option>
-                                                        </>
-                                                    )}
-                                                </select>
-                                                <InputType labelText="Donor Email"
-                                                    labelFor={'donorEmail'}
-                                                    inputType={'email'}
-                                                    value={user.email}
-                                                    onChange={(e) => setDonorEmail(e.target.value)}
-                                                />
-                                                <InputType labelText="Quantity"
-                                                    labelFor={'quantity'}
-                                                    inputType={'Number'}
-                                                    value={quantity}
-                                                    onChange={(e) => setQuantity(e.target.value)}
-                                                />
-                                            </>
-                                        )}
+            <div style={{textAlign: 'right', marginLeft: 300}}>
+                <img src="./assets/images/donateicon.png" classname="img-fluid" alt="Wild Landscape" />
+            </div>
 
-                                    </MDBModalBody>
-                                    <MDBModalFooter>
-                                        <button type='button' className="btn btn-secondary" onClick={toggleOpen}>
-                                            Close
-                                        </button>
-                                        <button type='button' className="btn btn-primary" onClick={toggleOpen}>
-                                            Submit
-                                        </button>
-                                    </MDBModalFooter>
-                                </MDBModalContent>
-                            </MDBModalDialog>
-                        </MDBModal>
+            <p className="fw-bold" style={{ fontSize: 45, textAlign: 'start', color: 'maroon', paddingLeft: '30px' }}>Donate Now</p>
 
-                    </Container>
-                </div >
-            </div >
-        </>
-    );
-}
+            <div style={{ width: '83vw', height: '70vh', position: 'relative' }}>
+              {/* Zoom effect */}
+              <style dangerouslySetInnerHTML={{__html: `
+                .zoom-effect {
+                  overflow: hidden;
+                  position: relative;
+                  width: 100%;
+                  height: 100%;
+                }
 
-export default DonationPage
+                .zoom-effect img {
+                  transition: transform 0.3s ease;
+                  width: 100%;
+                  height: 100%;
+                  object-fit: cover; /* Maintain aspect ratio and cover the entire container */
+                }
+
+                .zoom-effect:hover img {
+                  transform: scale(1.2); /* Zoom effect on hover */
+                }
+
+                .mask {
+                  position: absolute;
+                  top: 0;
+                  left: 0;
+                  width: 100%;
+                  height: 100%;
+                  background-color: rgba(0, 0, 0, 0.5); /* Adjust opacity as needed */
+                  display: flex;
+                  justify-content: center;
+                  align-items: center;
+                }
+
+                .mask p {
+                  color: black;
+                  font-size: 24px;
+                }
+              `}} />
+              <div className="zoom-effect">
+                <img src="./assets/images/person1.png" alt="person1" />
+                <div className="mask">
+                </div>
+              </div>
+            </div>
+            
+            {/* <div>
+              <img src="https://mdbootstrap.com/img/new/textures/small/52.jpg" className="float-none" />
+              <p className="mb-5">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus reiciendis alias
+                asperiores molestiae veniam
+                nihil dolore quaerat libero voluptatibus, officia, ut ea deserunt itaque debitis assumenda, aliquid
+                rerum officiis
+                commodi? Nobis perspiciatis consequatur vero aut, placeat praesentium quam eveniet. A amet provident
+                perferendis
+                fugit, alias beatae unde qui velit dignissimos quidem architecto doloribus maiores quis ut.
+                Eligendi, culpa,
+                repudiandae sint quidem molestiae vero dolor cumque fugit aliquid ipsam voluptates distinctio.
+                Corporis animi laborum
+                nostrum labore similique ullam, optio quidem quae adipisci iste at molestiae, delectus voluptates
+                deleniti sint
+                molestias modi itaque deserunt officiis asperiores aliquam. Vitae nostrum autem animi assumenda!
+              </p>
+            </div> */}
+          </Col>
+        </Row>
+        <Row className="mt-5 card-container">
+          <Col md={4} className="order-md-first" style={{ marginLeft: '200px', marginRight: '500px' }}>
+            <MDBCard>
+              <MDBCardImage
+                src='https://mdbootstrap.com/img/new/standard/city/041.webp'
+                alt='...'
+                position='top'
+              />
+              <MDBCardBody>
+                <MDBCardTitle>Card title</MDBCardTitle>
+                <MDBCardText>
+                  This is a longer card with supporting text below as a natural lead-in to additional content.
+                  This content is a little bit longer.
+                </MDBCardText>
+              </MDBCardBody>
+            </MDBCard>
+          </Col>
+        </Row>
+
+        
+        
+      </Container>
+    </>
+  );
+};
+
+export default Campain;
